69b13d11c9c4c34f854418838eb8e9204b1d1b12
shinkoou <uzumakiudyr835@gmail.com>
2026-09-03 21:58:42 -0300

    Update ShyxSeekApp.kt

--- app/src/main/java/com/shyxseek/app/ui/ShyxSeekApp.kt
+++ app/src/main/java/com/shyxseek/app/ui/ShyxSeekApp.kt
@@ -18,7 +18,6 @@ import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -62,7 +61,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberSaveable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
